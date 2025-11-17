27c7173a21302e1df69a64cc6e04bd9e5c3f16b1
Mehedi222 <shantomehedihasan2001@gmail.com>
2025-11-17 10:32:56 +0600

    add formatValue ,getLenth,person and fillterByRating

--- solution.ts
+++ solution.ts
@@ -30,3 +30,38 @@ console.log(getLength([10, 20, 30, 40]));
 
 
 
+class person {
+    name: string;
+    age: number;
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+    }
+    getDetails(): string {
+        return `Name: ${this.name}, Age: ${this.age}`;
+    }
+}
+const person1 = new person('John Doe', 30);
+console.log(person1.getDetails());
+
+const person2 = new person('Alice', 25);
+console.log(person2.getDetails());
+
+
+
+interface Item {
+    title: string;
+    rating: number;
+}
+function filterByRating(items: Item[]): Item[] {
+    return items.filter(item => item.rating >= 4);
+}
+const books = [
+    { title: 'Book A', rating: 4.5 },
+    { title: 'Book B', rating: 3.2 },
+    { title: 'Book C', rating: 5.0 },
+];
+
+console.log(filterByRating(books));
+
+
